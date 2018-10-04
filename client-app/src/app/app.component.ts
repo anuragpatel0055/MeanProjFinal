0d44cdb1fcc7fdc9490e1666f8c318272b1a464b
unknown <anuragpatel0055@gmail.com>
2018-10-04 20:14:59 +0530

    proj initiate

---
+++ client-app/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { TestServiceService } from './test-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client-app';
+  JsonData:string;
+  constructor(private test: TestServiceService){
+    this.getData();
+    }
+
+
+  getData() {
+   var JsonData;
+    var t= this.test.getConfig('http://localhost:3000/api/student');
+    t.subscribe(users => {this.JsonData= JSON.stringify(users)
+
+      console.log(this.JsonData);
+    });
+    setTimeout(()=>{
+      console.log(JsonData);
+ }, 1000);
+
+  }
+  }
+
